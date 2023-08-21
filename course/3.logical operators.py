7a2d694748b07066942786590c53bceb496e8581
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-08-21 15:22:58 +0300

    logical operators

---
+++ course/3.logical operators.py
@@ -0,0 +1,27 @@
+# and
+# salary = int(input('How much do you wanna get paid per month? : '))
+# experience = int(input('How much year of experience do you have? : '))
+# if salary >=0 and experience>=0 :
+#     print('Nasib')
+# else: 
+#     print('Are you serius bruh ?')
+
+# # or
+# job_salary = int(input('You salary will be : '))
+# job_location = input('You will be working in the : ')
+# job_position = input('You need to know : ')
+# if job_salary>=2000 or job_location=='US' or job_position=='CEO':
+#     print('deal')
+# else: 
+#     print('next')
+
+
+# not
+# if not age<12    #the same stuff
+# if age>-12       #the same stuff
+
+
+
+
+print('Priority is as follows :')
+print('not', 'and', 'or', sep='\n')
