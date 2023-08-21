fb8eec88e6e98d1934956869aa5369184a6c39a7
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-08-21 14:46:17 +0300

    add new

---
+++ course/1.sep+end.py
@@ -0,0 +1,10 @@
+#end
+print('I am', end='...')
+print('what', end='???', sep='!')
+
+#sep
+print('I', 'am', 'a', 'student', sep='%')
+
+
+print('I', 'am', 'a', 'student', sep='\n')
+
---
+++ course/3.input+print.py
@@ -0,0 +1,6 @@
+#print
+print('Dady came back and gonna achive his goal !')
+
+#input
+name = input()
+print('I am ', name)
