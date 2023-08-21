e33d7a572fec43c2d896b838750e5ec76a7893d3
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-08-22 02:05:47 +0300

    i did it

--- course/test.py
+++ course/test.py
@@ -1,12 +1,28 @@
-a, b= input(), input()
-if (a!='красный' and a!='синий' and a!='желтый') or (b!='красный' and b!='синий' and b!='желтый'):
-    print('ошибка цвета')
-elif a==b:
-    print(a)
-elif (a=='красный' and b=='синий') or ((b=='красный' and a=='синий')):
-    print('фиолетовый')
-elif (a=='красный' and b=='желтый') or ((b=='красный' and a=='желтый')):
-    print('оранжевый')
-elif (a=='желтый' and b=='синий') or ((b=='желтый' and a=='синий')):
-    print('зеленый')
+a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
+if (a1<a2 and b1<b2 and b1<a2) or (a2<a1 and b2<b1 and b2<a1) :
+    print('пустое множество')
+elif a1<a2 and b2<b1:
+    print(a2, b2)
+elif a2<a1 and b1<b2:
+    print(a1, b1)
+elif a1<a2 and b1<b2 and a2<b1:
+    print(a2, b1)
+elif a2<a1 and b2<b1 and a1<b2:
+    print(a1, b2)
+elif a2==b1 and a1<b2:
+    print(a2)
+elif a1==b2 and a2<b1:
+    print(b2)
+elif a1==b1 and a2==b2:
+    print(a1, a2)
+elif b1==b2 and a2<a1:
+    print(a1, b1)
+elif a1==a2 and b1<b2: #done
+    print(a1, b1)
+elif b1==b2 and a1<b1: #done
+    print(a2, b2)
+elif a2==b2 and b1<a1:
+    print(a1, a2)
+
+
 
