24c141cee4378cbb43e702bdc16e27383f26b641
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-08-21 14:49:45 +0300

    operator finished

--- course/2.operators.py
+++ course/2.operators.py
@@ -6,5 +6,11 @@ print(123 % 2)
 print(123 // 2)
 
 
-# finding the last digit of 4 digit number
+# finding the first digit of 4 digit number
 print(1234 // 1000)
+# finding the second digit of 4 digit number
+print(1234 // 100 % 10)
+# finding the third digit of 4 digit number
+print(1234 // 10 % 10)
+# finding the last digit of 4 digit number
+print(1234 % 10)
