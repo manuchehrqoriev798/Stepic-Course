edc51f225ba22323d43ddbf0b2927083dfdd2266
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-08-22 17:02:58 +0300

    str

--- course/test.py
+++ course/test.py
@@ -1,2 +1,3 @@
-a, b, c, d = float(input()), float(input()), float(input()), float(input())
-print(abs(-a-c)+abs(b-d))
+p1, p2, q1, q2 = float(input()), float(input()), float(input()), float(input())
+m = abs(p1-q1)+abs(p2-q2)
+print(m)
