67c80375e2d8b5f87729d5741f1eb27b2ac8c4d4
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-09-02 10:27:15 +0600

    review 6

--- course/test.py
+++ course/test.py
@@ -1,12 +1,7 @@
-num = int(input())
-max_digit = -1
-while num != 0:
-    digit = num % 10
-    if digit % 3 == 0:
-        if digit > max_digit:
-            max_digit = digit
-    num = num // 10
-if max_digit == -1:
-    print('NO')
-else:
-    print(max_digit)
+n = int(input())
+product = 1
+while n > 0:
+    last_digit = n % 10
+    n //= 10
+    product *= last_digit
+print(product)
