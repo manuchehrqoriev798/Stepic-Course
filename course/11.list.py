edb52445c2c15d09c7a92202cc45c8b5a405f1fc
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-09-11 09:18:21 +0600

    list

--- course/11.list.py
+++ course/11.list.py
@@ -2,3 +2,8 @@ numbers = [2, 3, 4, 5, 6]
 letters = ['a', 'b', 'c', 'd', 'e']
 print(numbers[0])
 print(letters[2])
+
+
+myList = []     # empty list
+myList = list() # empty list 
+
---
+++ course/11.list_range.py
@@ -0,0 +1,6 @@
+# myList = [range(5)]
+numbers = list(range(5))
+print(numbers)
+even_numbers = list(range(0, 10, 2))
+odd_numbers = list(range(1, 10, 2))
+print(even_numbers, odd_numbers, sep='\n')
