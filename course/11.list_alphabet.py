f6318a737b46f58a26cae2fe507639ffdd3b1304
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-09-14 16:54:21 +0600

    list

---
+++ course/11.list_alphabet.py
@@ -0,0 +1,4 @@
+new_list = []
+for i in range(1, 27):
+    new_list.append((chr(64+i)*i).lower())
+print(new_list)
---
+++ course/11.list_tsikle.py
@@ -0,0 +1,5 @@
+num = int(input())
+new_list = []
+for i in range(0, num):
+    new_list.append(input()) 
+print(new_list)
